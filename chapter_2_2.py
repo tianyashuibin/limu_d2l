054eb362ad587a61ea85571f7e4e95a84cf3c5da
bee-eater <minim4@bee-eater-mhyMac-mini.local>
2025-07-24 10:34:31 +0800

    init

---
+++ chapter_2_2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import torch
+
+data = pd.read_csv("house_tiny.csv")
+print( data)
+
+inputs = data.iloc[:, 0:2]
+outputs = data.iloc[:, 2]
+print(inputs)
+print(outputs)
+
+inputs["NumRooms"] = inputs["NumRooms"].fillna(inputs["NumRooms"].mean())
+
+inputs = pd.get_dummies(inputs, dummy_na=True)
+print( inputs)
+
+inputs[["Alley_Pave", "Alley_nan"]] = inputs[["Alley_Pave", "Alley_nan"]].astype(int)
+print(inputs)
+
+x = torch.tensor(inputs.to_numpy(dtype=float))
+y = torch.tensor(outputs.to_numpy(dtype=float))
+print(x, y)
