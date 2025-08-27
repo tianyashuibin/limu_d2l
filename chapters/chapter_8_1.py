d3b47a75c06318b794a13c962611571d9136bc44
bee-eater <minim4@bee-eater-mhyMac-mini.local>
2025-08-27 12:00:24 +0800

    update chapter_8_1

--- chapters/chapter_8_1.py
+++ chapters/chapter_8_1.py
@@ -68,4 +68,20 @@ d2l.plot([time_np, time_np[tau:], time_np[n_train + tau:]],
           multistep_preds[n_train + tau:].detach().numpy()], 'time',
          'x', legend=['data', '1-step preds', 'multistep preds'],
          xlim=[1, 1000], figsize=(6, 3))
+# plt.show()
+
+# 多步预测
+max_steps = 64
+features = torch.zeros((T - tau - max_steps + 1, tau + max_steps))
+for i in range(tau):
+    features[:, i] = x[i: i + T - tau - max_steps + 1]
+
+for i in range(tau, tau + max_steps):
+    features[:, i] = net(features[:, i - tau:i]).reshape(-1)
+
+steps = (1, 4, 16, 64)
+d2l.plot([time_np[tau + i - 1: T - max_steps + i] for i in steps],
+         [features[:, (tau + i - 1)].detach().numpy() for i in steps], 'time', 'x',
+         legend=[f'{i}-step preds' for i in steps], xlim=[5, 1000],
+         figsize=(6, 3))
 plt.show()
